6060214e6002eda7032161d85ab11c1e099ce861
bearpolax <49691742+bearpolax@users.noreply.github.com>
2019-09-20 08:59:30 -0300

    Add files via upload

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { FirebaseUIModule } from 'firebaseui-angular';
 
 @NgModule({
   imports: [
     IonicModule,
+    FirebaseUIModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public afAuth: AngularFireAuth,) {
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut().then(() => {
+    location.reload();
+
+  });
+  }
 
 }
